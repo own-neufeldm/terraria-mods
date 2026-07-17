b353d166fb39f611cd526566ae7cc0a271da7e43
Martin Neufeld <m.neufeld946@gmail.com>
2026-07-17 22:37:12 +0200

    Add sound options

--- MemeSounds/Common/Configs/MemeSoundsClientConfig.cs
+++ MemeSounds/Common/Configs/MemeSoundsClientConfig.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using Newtonsoft.Json;
@@ -13,6 +14,12 @@ namespace MemeSounds.Common.Configs
     Spawn,
   }
 
+  [AttributeUsage(AttributeTargets.Field)]
+  public class SoundOptionsAttribute(float volume) : Attribute
+  {
+    public float Volume { get; } = volume;
+  }
+
   public class MemeSoundsClientConfig : ModConfig
   {
     public override ConfigScope Mode => ConfigScope.ClientSide;
@@ -25,30 +32,39 @@ namespace MemeSounds.Common.Configs
 
     [Header("SoundEffects")]
 
+    [SoundOptions(volume: 1f)]
     [DefaultValue(MemeSoundsEvent.Death)]
     public MemeSoundsEvent Ack;
 
+    [SoundOptions(volume: 1f)]
     [DefaultValue(MemeSoundsEvent.Death)]
     public MemeSoundsEvent Fah;
 
+    [SoundOptions(volume: 1f)]
     [DefaultValue(MemeSoundsEvent.Death)]
     public MemeSoundsEvent Knocked;
 
+    [SoundOptions(volume: 1f)]
     [DefaultValue(MemeSoundsEvent.Death)]
     public MemeSoundsEvent MagicFlute;
 
+    [SoundOptions(volume: 1f)]
     [DefaultValue(MemeSoundsEvent.Death)]
     public MemeSoundsEvent MetalPipe;
 
+    [SoundOptions(volume: 1f)]
     [DefaultValue(MemeSoundsEvent.Death)]
     public MemeSoundsEvent ReverbFart;
 
+    [SoundOptions(volume: 1f)]
     [DefaultValue(MemeSoundsEvent.Death)]
     public MemeSoundsEvent WrestlingBell;
 
+    [SoundOptions(volume: 1f)]
     [DefaultValue(MemeSoundsEvent.Death)]
     public MemeSoundsEvent SystemShutdown;
 
+    [SoundOptions(volume: 1f)]
     [DefaultValue(MemeSoundsEvent.Spawn)]
     public MemeSoundsEvent SystemStartup;
 
@@ -63,7 +79,15 @@ namespace MemeSounds.Common.Configs
         var onEvent = (MemeSoundsEvent)field.GetValue(this);
         if (onEvent == MemeSoundsEvent.Disabled) continue;
 
-        var sound = new SoundStyle($"MemeSounds/Assets/Sounds/{field.Name}").WithVolumeScale(0.3f);
+        var options = (SoundOptionsAttribute)Attribute.GetCustomAttribute(
+          field,
+          typeof(SoundOptionsAttribute)
+        );
+        var sound = new SoundStyle($"MemeSounds/Assets/Sounds/{field.Name}")
+        {
+          Volume = options.Volume
+        };
+
         if (onEvent == MemeSoundsEvent.Death) DeathSounds.Add(sound);
         if (onEvent == MemeSoundsEvent.Spawn) SpawnSounds.Add(sound);
       }

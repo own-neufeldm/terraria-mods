aa43ec1820bfd445d9a07247dd2eb9895ca4cdac
Martin Neufeld <m.neufeld946@gmail.com>
2026-07-19 22:12:04 +0200

    Ensure python is not run by accident

--- BetterTooltips/Tools/fetch.ps1
+++ BetterTooltips/Tools/fetch.ps1
@@ -1,4 +1,4 @@
 uv venv .venv --python 3.14 --allow-existing 2>&1 | Out-Null
 .venv\Scripts\activate 2>&1 | Out-Null
 uv pip install -r requirements.txt 2>&1 | Out-Null
-python fetch.py $args
+python _fetch.py $args
